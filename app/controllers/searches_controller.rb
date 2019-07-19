1b3e461a83d2919b8ee4a1f291f5bbb7f3f6aa22
古賀克磨 <simesime3/cook>
2019-07-19 21:32:42 +0900

    検索と編集ページ

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,8 @@ class SearchesController < ApplicationController
   end
 
   def index
-    @q = Recipe.ransack(params[:q])
+    params[:q][:title_cont] = params[:q][:title_cont].split(/[\p{blank}\s]+/)
+    @q = Recipe.ransack(title_cont_any: params[:q][:title_cont])
     @recipes = @q.result(distinct: :true)
   end
 end
